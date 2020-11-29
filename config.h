7d5a5296b65e9d5d62392bb4fbcf1b0388918ee6
lime <osmium760@gmail.com>
2020-11-29 17:22:22 +0300

    new colors

--- config.h
+++ config.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	             /*     fg         bg       */
+	[SchemeNorm] = { "#f3f4f5", "#0d1b2a" },
+	[SchemeSel] = { "#f3f4f5", "#3399ff" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
